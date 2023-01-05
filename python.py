be0f285a563a51aa41ed28301bf7705e09e660a1
BhavikJoshi20405 <121996078+BhavikJoshi20405@users.noreply.github.com>
2023-01-05 09:44:00 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,16 @@
+class cal :
+    num1=int(input("Enter a number : "))
+    num2=int(input("Enter a number : "))
+def add(self):
+    print (self.num1 + self.num2)
+def sub(self):
+    print (self.num1 - self.num2)
+def multi(self):
+    print (self.num1 * self.num2)
+def div(self):
+     print (self.num1 / self.num2)
+var = cal()
+var = add()
+var = sub()
+var = multi()
+var = div()
